b7905a654c0fb1100ea1106508eb42d11068ab21
Aurélien Gâteau <mail@agateau.com>
2015-04-15 15:43:17 +0200

    spaces

--- git.py
+++ git.py
@@ -2,9 +2,11 @@ import logging
 
 from subprocess import check_call
 
+
 def clone(source_url, dst):
     logging.info('Cloning %s', source_url)
     check_call(['git', 'clone', source_url, dst])
+
 
 def update(worktree_dir, commit_id):
     logging.info('Updating %s to %s', worktree_dir, commit_id)
