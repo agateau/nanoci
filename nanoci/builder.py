2fef0a60f6a6423a7279bc9fd42ffaf340b67787
Aurélien Gâteau <mail@agateau.com>
2015-04-26 12:14:08 +0200

    Factorize log file opening

--- nanoci/builder.py
+++ nanoci/builder.py
@@ -34,7 +34,7 @@ class Builder(object):
             except OSError:
                 self.build_id += 1
 
-        self.log_file_path = os.path.join(self.log_dir, Builder.BUILD_LOG_NAME)
+        self.log_fp = None
 
     @staticmethod
     def get_log_base_dir(config, project_name):
@@ -48,18 +48,16 @@ class Builder(object):
         # We do not use the logging module because we want to have one single
         # log file containing our messages and the output of the build steps
         timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        with open(self.log_file_path, 'a') as fp:
-            fp.write('# ' + timestamp + ': ' + message + '\n')
-            fp.flush()
+        self.log_fp.write('# ' + timestamp + ': ' + message + '\n')
+        self.log_fp.flush()
 
     def check_source(self):
         self.log('Running step "git"')
         source_url = read_path(self.project['source']['url'])
-        with open(self.log_file_path, 'a') as fp:
-            if not os.path.isdir(self.src_dir):
-                git.clone(fp, source_url, self.src_dir)
-            git.update(fp, self.src_dir, self.commit_id)
-            fp.flush()
+        if not os.path.isdir(self.src_dir):
+            git.clone(self.log_fp, source_url, self.src_dir)
+        git.update(self.log_fp, self.src_dir, self.commit_id)
+        self.log_fp.flush()
 
     def run_steps(self, step_type):
         """
@@ -82,25 +80,26 @@ class Builder(object):
             self.log('Running step "{}"'.format(name))
             script = step['script']
             try:
-                with open(self.log_file_path, 'a') as fp:
-                    fp.write('## Script\n{}\n## Output\n'.format(script.strip()))
-                    fp.flush()
-                    log_check_call(fp, script, shell=True, env=env, cwd=self.src_dir)
+                self.log_fp.write('## Script\n{}\n## Output\n'.format(script.strip()))
+                self.log_fp.flush()
+                log_check_call(self.log_fp, script, shell=True, env=env, cwd=self.src_dir)
             except CalledProcessError as exc:
                 self.log('Command failed with exit code {}'.format(exc.returncode))
                 return False
         return True
 
     def build(self):
-        self.log('Starting build #{}'.format(self.build_id))
-        try:
-            self.check_source()
-            ok = self.run_steps('build')
-            self.status = STATUS_SUCCESS if ok else STATUS_FAILURE
-        except Exception as exc:
-            trace = traceback.format_exc()
-            self.log('Build failed with an exception:\n{}'.format(trace))
-            self.status = STATUS_FAILURE
-        finally:
-            self.log('Build #{} finished: {}'.format(self.build_id, self.status))
-            self.run_steps('notify')
+        log_file_path = os.path.join(self.log_dir, Builder.BUILD_LOG_NAME)
+        with open(log_file_path, 'w') as self.log_fp:
+            self.log('Starting build #{}'.format(self.build_id))
+            try:
+                self.check_source()
+                ok = self.run_steps('build')
+                self.status = STATUS_SUCCESS if ok else STATUS_FAILURE
+            except Exception as exc:
+                trace = traceback.format_exc()
+                self.log('Build failed with an exception:\n{}'.format(trace))
+                self.status = STATUS_FAILURE
+            finally:
+                self.log('Build #{} finished: {}'.format(self.build_id, self.status))
+                self.run_steps('notify')
