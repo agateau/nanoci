c751cdf83bb01300b2602281a7e7dc8eedde2d55
Aurélien Gâteau <mail@agateau.com>
2015-04-28 20:11:16 +0200

    gitstep: returns False in case of errors

--- nanoci/gitstep.py
+++ nanoci/gitstep.py
@@ -1,5 +1,7 @@
 import os
 
+from subprocess import CalledProcessError
+
 from nanoci.fileutils import read_path
 from nanoci.step import Step
 from nanoci.subproclog import log_check_call
@@ -14,12 +16,15 @@ class GitStep(Step):
         url = read_path(self._arguments['url'])
         src_dir = env['SRC_DIR']
 
-        if os.path.isdir(src_dir):
-            log_check_call(log_fp, ['git', 'fetch'], cwd=src_dir)
-        else:
-            log_check_call(log_fp, ['git', 'clone', url, src_dir])
+        try:
+            if os.path.isdir(src_dir):
+                log_check_call(log_fp, ['git', 'fetch'], cwd=src_dir)
+            else:
+                log_check_call(log_fp, ['git', 'clone', url, src_dir])
 
-        log_check_call(log_fp, ['git', 'checkout', env['COMMIT_ID']], cwd=src_dir)
-
-        log_fp.flush()
-        return True
+            log_check_call(log_fp, ['git', 'checkout', env['COMMIT_ID']], cwd=src_dir)
+            return True
+        except CalledProcessError as exc:
+            return False
+        finally:
+            log_fp.flush()
