89bcf65b8ae9636b868c30abf700283c364cd94a
Aurélien Gâteau <mail@agateau.com>
2015-07-17 10:42:56 +0200

    Add new '-l' option to nanoci_log

--- nanoci/nanoci_log.py
+++ nanoci/nanoci_log.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
+import argparse
 import os
 import sys
-import argparse
+import time
 
 from nanoci.config import Config
 from nanoci.builder import Builder
@@ -11,10 +12,30 @@ DESCRIPTION = """\
 Show build logs for a project
 """
 
+def read_file_end(filename, linecount):
+    content = open(filename, 'rt').readlines()
+    return ''.join(content[-linecount:]).strip()
+
+
+def list_builds(log_base_dir):
+    ids = [int(x) for x in os.listdir(log_base_dir) if x.isdigit()]
+    for id in sorted(ids):
+        print()
+        print(id, end=': ')
+        log_file = os.path.join(log_base_dir, str(id), 'build.log')
+        if not os.path.exists(log_file):
+            print('No log file!')
+            continue
+        statinfo = os.stat(log_file)
+        print(time.ctime(statinfo.st_mtime))
+        print(read_file_end(log_file, 4))
+
 
 def main():
     parser = argparse.ArgumentParser()
     parser.description = DESCRIPTION
+    parser.add_argument('-l', '--list', dest='lst', action='store_true',
+                        default=False, help='List build results')
     parser.add_argument('name', help='Name of project')
     parser.add_argument('build_id', help='Build number (defaults to last)',
                         nargs='?')
@@ -26,6 +47,11 @@ def main():
         parser.error('Invalid project name {}'.format(project_name))
 
     log_base_dir = Builder.get_log_base_dir(config, project_name)
+
+    if args.lst:
+        list_builds(log_base_dir)
+        return 0
+
     if args.build_id is None:
         ids = [int(x) for x in os.listdir(log_base_dir) if x.isdigit()]
         build_id = str(max(ids))
